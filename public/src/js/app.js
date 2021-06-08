f25b73670230698af3f2a31823fdf099da50faa8
Maria Zayed <m.zayed@bisan.com>
2021-06-08 16:39:58 +0300

    Sending POST request using fetch method

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -51,3 +51,24 @@ fetch('https://httpbin.org/ip')
 	.catch(err => {
 		console.log("ERR", err)
 	})
+
+// API reference: http://httpbin.org/
+fetch('https://httpbin.org/post', {
+	method: "POST",
+	headers: {
+		'Content-type': 'application/json',
+		'Accept': 'application/json'
+	},
+	mode: 'cors',
+	body: JSON.stringify({message: "Does this work?"})
+})
+	.then(response => {
+		console.log("RES", response)
+		return response.json()
+	})
+	.then(response => {
+		console.log(response)
+	})
+	.catch(err => {
+		console.log("ERR", err)
+	})
