2d065f9e90c9e3dba690779938f8210022f64650
Maria Zayed <m.zayed@bisan.com>
2021-06-10 14:22:32 +0300

    Added static + dynamic caches

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,46 @@
 self.addEventListener('install', (event) => {
-	// console.log("Installing service worker", event)
+	event.waitUntil(
+		caches.open('static')
+		      .then((cache) => {
+			      console.log("Precaching app shell");
+			      cache.addAll(['/',
+			                    '/index.html',
+			                    '/src/js/app.js',
+			                    '/src/js/extra/promise.js',
+			                    '/src/js/extra/fetch.js',
+			                    '/src/js/extra/material.min.js',
+			                    '/src/css/app.css',
+			                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+			                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+			                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'])
+		      })
+		      .catch()
+	)
 })
 
 self.addEventListener('activate', (event) => {
-	// console.log("Activating service worker", event)
 	return self.clients.claim()
 })
 
 self.addEventListener('fetch', (event) => {
-	// console.log("Fetching something", event)
-	event.respondWith(fetch(event.request))
+	event.respondWith(
+		caches.match(event.request)
+		      .then((response) => {
+			      if (response) {
+				      return response
+			      } else {
+				      return fetch(event.request)
+					      .then(res => {
+						      return caches.open('dynamic')
+						                   .then(cache => {
+							                   cache.put(event.request.url, res.clone())
+							                   return res
+						                   })
+						                   .catch()
+					      })
+					      .catch()
+			      }
+		      })
+		      .catch()
+	)
 })
--- public/src/js/app.js
+++ public/src/js/app.js
@@ -42,3 +42,36 @@ function plusButtonClicked() {
 
 let plusButton = document.querySelector('#plus-button');
 plusButton.addEventListener('click', plusButtonClicked);
+
+
+var sharedMomentsArea = document.querySelector('#shared-moments');
+
+function createCard() {
+	var cardWrapper = document.createElement('div');
+	cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+	var cardTitle = document.createElement('div');
+	cardTitle.className = 'mdl-card__title';
+	cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
+	cardTitle.style.backgroundSize = 'cover';
+	cardTitle.style.height = '180px';
+	cardWrapper.appendChild(cardTitle);
+	var cardTitleTextElement = document.createElement('h2');
+	cardTitleTextElement.className = 'mdl-card__title-text';
+	cardTitleTextElement.textContent = 'San Francisco Trip';
+	cardTitle.appendChild(cardTitleTextElement);
+	var cardSupportingText = document.createElement('div');
+	cardSupportingText.className = 'mdl-card__supporting-text';
+	cardSupportingText.textContent = 'In San Francisco';
+	cardSupportingText.style.textAlign = 'center';
+	cardWrapper.appendChild(cardSupportingText);
+	componentHandler.upgradeElement(cardWrapper);
+	sharedMomentsArea.appendChild(cardWrapper);
+}
+
+fetch('https://httpbin.org/get')
+	.then(function (res) {
+		return res.json();
+	})
+	.then(function (data) {
+		createCard();
+	});
