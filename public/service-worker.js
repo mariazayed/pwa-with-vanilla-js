3f65eec0fcdda44dc085cfc49dd48956606c6117
Maria Zayed <m.zayed@bisan.com>
2021-08-26 15:23:14 +0300

    Connected with firebase

--- public/service-worker.js
+++ public/service-worker.js
@@ -94,7 +94,7 @@ self.addEventListener('fetch', (event) => {
 						      .then(res => {
 							      return caches.open('dynamic')
 							                   .then(cache => {
-								                   cache.put(event.request.url, res.clone())
+								                   // cache.put(event.request.url, res.clone())
 								                   return res
 							                   })
 							                   .catch()
