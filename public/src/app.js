9f8b329a03bd3f839590544e2ebc669aa0ba5ce8
Maria Zayed <mariawzayed@gmail.com>
2021-09-02 09:22:44 +0300

    Refactoring

--- public/src/app.js
+++ public/src/app.js
@@ -3,31 +3,6 @@ if (!window.Promise) {
 	window.Promise = Promise
 }
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-// 	apiKey: "AIzaSyC2l_vkZzvYTMMTE1tFI_6aYg03rSpaPz0",
-// 	authDomain: "pwa-vanilla-js.firebaseapp.com",
-// 	projectId: "pwa-vanilla-js",
-// 	storageBucket: "pwa-vanilla-js.appspot.com",
-// 	messagingSenderId: "800674186660",
-// 	appId: "1:800674186660:web:f1922ee22922814e916d31"
-// };
-
-// Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-// const db = firebase.firestore()
-// const colRef = db.collection('posts')
-// const postsSnapshot = colRef.get();
-// postsSnapshot.then(res => {
-// 	             isNetworkDataReceived = true
-// 	             const postsList = res.docs.map(res1 => res1.data());
-// 	             console.log("postsList", postsList);
-// 	             updateUI(postsList)
-//              })
-//              .catch(err => {
-// 	             console.log("err", err);
-//              })
-
 
 // Checking if the service worker available in the browser
 if ('serviceWorker' in navigator) {
