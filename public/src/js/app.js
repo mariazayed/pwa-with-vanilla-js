53aab4c484e980434671fffd21062b8a2abf6596
Maria Zayed <mariawzayed@gmail.com>
2021-09-14 16:55:58 +0300

    Added notifications

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,12 +1,13 @@
-var deferredPrompt;
+let deferredPrompt;
+let enableNotificationButtons = document.querySelectorAll(".enable-notifications");
 
 if (!window.Promise) {
 	window.Promise = Promise;
 }
 
-if ('serviceWorker' in navigator) {
+if ("serviceWorker" in navigator) {
 	navigator.serviceWorker
-	         .register('/service-worker.js')
+	         .register("/service-worker.js")
 	         .then(function () {
 		         // console.log('Service worker registered!');
 	         })
@@ -15,9 +16,54 @@ if ('serviceWorker' in navigator) {
 	         });
 }
 
-window.addEventListener('beforeinstallprompt', function (event) {
+if ("Notification" in window) {
+	for (let i = 0; i < enableNotificationButtons.length; i++) {
+		enableNotificationButtons[i].style.display = "inline-block";
+		enableNotificationButtons[i].addEventListener("click", askForNotificationPermission);
+	}
+}
+
+window.addEventListener("beforeinstallprompt", function (event) {
 	// console.log('beforeinstallprompt fired');
 	event.preventDefault();
 	deferredPrompt = event;
 	return false;
 });
+
+function askForNotificationPermission() {
+	// Push + Notification permission
+	Notification.requestPermission(result => {
+		console.log("User choice", result);
+		if (result !== "granted") {
+			console.log("No notification permission granted");
+		} else {
+			displayConfirmNotification();
+		}
+	});
+}
+
+function displayConfirmNotification() {
+	if ("serviceWorker" in navigator) {
+		navigator.serviceWorker.ready
+			// Active service worker
+			     .then((sw) => {
+				     const options = {
+					     body: "You successfully subscribed to our notification service",
+					     icon: "/src/images/icons/app-icon-96x96.png",
+					     image: "/src/images/icons/sf-boat.png",
+					     dir: "ltr",
+					     lang: "en-US", // BCP 47
+					     vibrate: [100, 50, 2000],
+					     badge: "/src/images/icons/app-icon-96x96.png"
+				     };
+				     sw.showNotification("Successfully Subscribed (from SW)!", options);
+			     });
+	}
+
+	// Without using the service workers
+	// const options = {
+	// 	body: "You successfully subscribed to our notification service"
+	// };
+	//
+	// new Notification("Successfully Subscribed!", options);
+}
