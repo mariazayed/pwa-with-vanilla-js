a9dde634529677d0399a54b910be4d0a3d94a800
Maria Zayed <mariawzayed@gmail.com>
2021-10-04 09:12:36 +0300

    Fixes

--- backend/app/app.ts
+++ backend/app/app.ts
@@ -22,7 +22,7 @@ class App {
     }
 
     private config(): void {
-	    // this.app.use(cors())
+	    this.app.use(cors())
 	    // support application/json type post data
 	    this.app.use(bodyParser.json());
 	    //support application/x-www-form-urlencoded post data
